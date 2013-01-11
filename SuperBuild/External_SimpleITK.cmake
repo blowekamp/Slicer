73d14b4936e6cdb930341be8e75a3186ef657d1c
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-01-11 08:11:39 -0700

    Enable SimpleITK Threads in python, and remove hash version
    The SimpleITK external project has been update to use a better
    convention of using variable for the repo and git hash. This has also
    been updated to refer to my SimpleITK repo with a commit to unlock the
    GIL in python. Additionally, for the official SimpleITK repo the git
    has is now removed from the version.

--- SuperBuild/External_SimpleITK.cmake
+++ SuperBuild/External_SimpleITK.cmake
@@ -44,11 +44,23 @@ configure_file(SuperBuild/SimpleITK_install_step.cmake.in
 set(SimpleITK_INSTALL_COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/SimpleITK_install_step.cmake)
 
 
+if(NOT DEFINED git_protocol)
+  set(git_protocol "git")
+endif()
+
+#set(SimpleITK_REPOSITORY "${git_protocol}://itk.org/SimpleITK.git")
+set(SimpleITK_REPOSITORY "${git_protocol}://github.com:blowekamp/SimpleITK.git")
+set(SimpleITK_GIT_TAG SIMPLEITK-348_PythonThreading)
+
+if ( SimpleITK_REPOSITORY MATCHES "itk\\.org/SimpleITK\\.git$")
+  set(SimpleITK_OFFICIAL 1)
+endif()
+
 ExternalProject_add(SimpleITK
   SOURCE_DIR SimpleITK
   BINARY_DIR SimpleITK-build
-  GIT_REPOSITORY http://itk.org/SimpleITK.git
-  GIT_TAG badde952e25e99db1c2af57c2903c36ca088b573
+  GIT_REPOSITORY git@github.com:blowekamp/SimpleITK.git
+  GIT_TAG SIMPLEITK-348_PythonThreading
   "${slicer_external_update}"
   CMAKE_ARGS
     -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
@@ -57,8 +69,11 @@ ExternalProject_add(SimpleITK
     -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
     ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-  # SimpleITK does not work with shared libs turned on
+  # The SimpleITK libraries are 10x bigger
   -DBUILD_SHARED_LIBS:BOOL=OFF
+  -DSimpleITK_PYTHON_THREADS:BOOL=ON
+  # if from official repo remove GIT hash from version
+  -DSimpleITK_BUILD_DISTRIBUTE:BOOL=${SimpleITK_OFFICIAL}
   -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}
   -DITK_DIR:PATH=${ITK_DIR}
   -DBUILD_EXAMPLES:BOOL=OFF
